8f0893da3bba11019bbfc1d5495f6767805da99a
khaledbakeer <bakeer@khaled.de>
2019-03-29 23:36:13 +0100

    routes and default views with an internal example in views.py how to show a list of posts using django blocks

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
+    url('about', views.about, name='blog_aboutPage'),
     url('', views.home, name='blog_homePage'),
-    url('about/', views.about, name='blog_aboutPage'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,29 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
 
+posts = [
+    {
+        'title': 'first Post',
+        'author': 'John Smith',
+        'post_date': '01.02.1999',
+        'content': 'content of first post'
+    },
+    {
+        'title': 'sevond Post',
+        'author': 'John SS',
+        'post_date': '02.05.2005',
+        'content': 'content of second post'
+    }
+]
 
-# Create your views here.
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About Page</h1>')
+    # return HttpResponse('<h1>Blog About Page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})

c40902271bad1e59ae544266c152bcc787e79dad
khaledbakeer <bakeer@khaled.de>
2019-03-31 00:26:27 +0100

    static path fix

--- kPro/settings.py
+++ kPro/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 

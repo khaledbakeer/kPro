09d3cb663cc89b91b79a37316b316b5a658354e2
khaledbakeer <bakeer@khaled.de>
2019-03-30 13:30:12 +0100

    registration completed with alert messages

--- users/views.py
+++ users/views.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, 'Account created for ' + username + '!')
+            # messages.debug()
+            # messages.info()
+            # messages.error()
+            # messages.success()
+            # messages.warning()
+            return redirect('blog_homePage')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/registration.html', {'form': form, 'title': 'Registration'})

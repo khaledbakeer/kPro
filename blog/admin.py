38f9a05c279ae7ff4643366a39ba0c97b0c10441
khaledbakeer <bakeer@khaled.de>
2019-03-31 00:16:55 +0100

    upgrade django

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from models import Post
+from blog.models import Post
 from django.contrib import admin
 
 admin.site.register(Post)
--- kPro/urls.py
+++ kPro/urls.py
@@ -13,23 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^register/', user_views.register, name='register'),
-    url(r'^profile/', user_views.profile, name='profile'),
-    url(r'^login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    url(r'^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    url(r'^', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', include('blog.urls')),
 ]
 
 # Serving static files during development:
 # https://docs.djangoproject.com/en/2.1/howto/static-files/#serving-static-files-during-development
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

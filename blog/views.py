a161e893a8c451c5d2c843eb90b16194d5e99932
khaledbakeer <bakeer@khaled.de>
2019-03-30 11:53:21 +0100

    load posts from the db

--- blog/views.py
+++ blog/views.py
@@ -3,26 +3,12 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render
-
-posts = [
-    {
-        'title': 'first Post',
-        'author': 'John Smith',
-        'date_posted': '01.02.1999',
-        'content': 'content of first post'
-    },
-    {
-        'title': 'sevond Post',
-        'author': 'John SS',
-        'date_posted': '02.05.2005',
-        'content': 'content of second post'
-    }
-]
+from models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 

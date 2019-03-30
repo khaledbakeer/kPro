f32c4b3670bda2545a76f80c7693c0b67730d99c
khaledbakeer <bakeer@khaled.de>
2019-03-30 12:45:20 +0100

    registration Template

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
-# Create your views here.
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/registration.html', {'form': form, 'title': 'Registration'})

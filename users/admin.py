b38d2ad6311f1361913adc4da2a9f89b4d615759
khaledbakeer <bakeer@khaled.de>
2019-03-30 22:22:39 +0100

    make Profile model and add users to the admin page

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from users.models import Profile
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Profile)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return self.user.username + 's Profile'

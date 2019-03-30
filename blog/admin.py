13380e7650303ee2425cb7ce34c31479f5d2be92
khaledbakeer <bakeer@khaled.de>
2019-03-30 12:12:10 +0100

    register Post model in Admin Panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from models import Post
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Post)

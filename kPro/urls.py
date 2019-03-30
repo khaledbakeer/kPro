c688d7c15adbb6ec24e2a3faa624244236d86bb2
khaledbakeer <bakeer@khaled.de>
2019-03-30 21:58:10 +0100

    create profile page with username output

--- kPro/urls.py
+++ kPro/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^register/', user_views.register, name='register'),
+    url(r'^profile/', user_views.profile, name='profile'),
     url(r'^login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     url(r'^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     url(r'^', include('blog.urls')),

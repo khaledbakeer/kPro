6b77d5e2ca3957966dc9000aed523861c252ef59
khaledbakeer <bakeer@khaled.de>
2019-03-30 11:43:44 +0100

    Post model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+
+    content = models.TextField()
+
+    date_updated = models.DateTimeField(auto_now=True)
+
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title

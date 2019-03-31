7ad6c0763e2855f3434ea1da3f37434ac9f8389f
khaledbakeer <bakeer@khaled.de>
2019-03-31 18:55:33 +0200

    delete a post successfully

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog_homePage'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post_delete'),
     path('about', views.about, name='blog_aboutPage'),
 
 ]

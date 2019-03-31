407d19a6a0290e6261a012a96bd2e3938a2a97a1
khaledbakeer <bakeer@khaled.de>
2019-03-31 16:21:32 +0200

    using ListView to show posts with help home def

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from .views import PostListView
 
 urlpatterns = [
     path('about', views.about, name='blog_aboutPage'),
-    path('', views.home, name='blog_homePage'),
+    path('', PostListView.as_view(), name='blog_homePage'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.views.generic import ListView
 from blog.models import Post
 
 
@@ -9,6 +10,13 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # antwort auf debug message von: <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # order the posts. The new one first. just add '-'
 
 
 def about(request):

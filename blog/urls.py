0f673aeb859d426e036ee6b79b04494e0bb20692
khaledbakeer <bakeer@khaled.de>
2019-03-31 18:27:56 +0200

    create a new post and return to new url using reverse to the detail post page

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog_homePage'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('about', views.about, name='blog_aboutPage'),
 
 ]

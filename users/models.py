1ef07761c344d4d09a9920dbbb81aa93e4313115
khaledbakeer <bakeer@khaled.de>
2019-03-31 15:43:24 +0200

    resize profile images

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -10,3 +11,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username + 's Profile'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)

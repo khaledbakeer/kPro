990a172016a780b022819b2a90cba9ee179dab45
khaledbakeer <bakeer@khaled.de>
2019-04-01 21:58:59 +0200

    migrated

--- kPro/settings.py
+++ kPro/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'subjects.apps.SubjectsConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',

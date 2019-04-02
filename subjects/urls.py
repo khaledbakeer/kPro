aa37005263e387670d472925afce6aa919f897f3
khaledbakeer <bakeer@khaled.de>
2019-04-02 02:00:36 +0200

    update

--- subjects/urls.py
+++ subjects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from subjects.views import SubjectListView, SubjectDetailView
+from subjects.views import SubjectListView, SubjectDetailView, SubjectPostListView
 
 urlpatterns = [
     path('', SubjectListView.as_view(), name='subject_homePage'),
     path('<int:pk>/', SubjectDetailView.as_view(), name='subject_detail'),
+    path('u/<str:subject_name>/', SubjectPostListView.as_view(), name='subject_posts'),
 ]
--- subjects/views.py
+++ subjects/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.models import User
 
 from subjects.models import Subject
+from blog.models import Post
 
 
 def home(request):
@@ -21,3 +23,17 @@ class SubjectListView(ListView):
 
 class SubjectDetailView(DetailView):  # one post
     model = Subject
+
+
+class SubjectPostListView(ListView):
+    """
+    all posts of a user
+    """
+    model = Post
+    template_name = 'subjects/subject_posts.html'  # antwort auf debug message von: <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        subject = get_object_or_404(Subject, subject_name=self.kwargs.get('subject_name'))
+        return Post.objects.filter(subject=subject).order_by('-date_posted')
